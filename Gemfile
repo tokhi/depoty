a844b978dfc1b7b36295ec5dacd1d0bc11dee690
shafi <shafi.tokhi@gmail.com>
2012-10-29 12:44:53 +0430

    sending orders via email

--- Gemfile
+++ Gemfile
@@ -4,9 +4,9 @@ gem 'rails', '3.2.3'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
-
+gem 'therubyracer'
 gem 'sqlite3'
-
+gem 'will_paginate', '>= 3.0.pre'
 
 # Gems used only for assets and not required
 # in production environments by default.
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,7 @@
 class Product < ActiveRecord::Base
+  default_scope :order => 'title'
+  has_many :line_items
+  has_many :orders, :through => :line_items
   attr_accessible :description, :image_url, :price, :title
   validates :title, :description, :image_url, :presence => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
@@ -6,5 +9,19 @@ class Product < ActiveRecord::Base
   validates :image_url, :format => {  :with => %r{\.(gif|jpg|png)$}i,
     :message => 'must be a URL for GIF, JPG or PNG image.'
   }
+  
+  before_destroy :ensure_not_referenced_by_any_line_item
+  #...
+  private
+  # ensure that there are no line items referencing this product
+  def ensure_not_referenced_by_any_line_item
+    if line_items.empty?
+    return true
+    else
+    errors.add(:base, 'Line Items present')
+    return false
+    end
+  end
+
 
 end
